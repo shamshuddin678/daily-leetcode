508b9738c679ef776467a5b607d67f82b0b716ba
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-07-06 22:21:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (51.57%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -8,6 +8,6 @@ class Solution(object):
         n = len(nums)
 
         for i in range(n):
-            if nums[i] > nums[(i + 1) % n]:
+            if nums[i] > nums[(i + 1) % n]: # Here % means comapare the last elelmet with the first element
                 count += 1
         return count <= 1
