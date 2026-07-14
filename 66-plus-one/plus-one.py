a78d8ca55995f02ac6f2e9778df05b30151048a8
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-07-14 08:19:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.23%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,6 +4,19 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        a = int("".join(map(str,digits))) # heer convert into number
+        '''
+        apprach 1:
+        a = int("".join(map(str,digits))) # here convert into number
         a += 1
-        return list(map(int,str(a)))
+        return list(map(int,str(a)))'''
+        # Traverse the list from the last element to the first
+        for i in range(len(digits)-1, -1, -1):
+            # If the current digit is 9, set it to 0
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                # If the current digit is not 9, increment it by 1 and return the list
+                digits[i] = digits[i] + 1
+                return digits
+        # If all digits are 9, prepend 1 to the list
+        return [1] + digits
