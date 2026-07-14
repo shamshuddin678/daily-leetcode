a1d9892635957eaa0b8c9573e93579056a4ae7bd
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-07-14 08:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (52.95%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,9 +5,13 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        '''
+        approach 1:
         i = 0
         for j in range(len(nums)):
             if(nums[j] != val):
                 nums[i] = nums[j]
                 i += 1
-        return i        
+        return i '''
+        while val in nums:
+            nums.remove(val)       
