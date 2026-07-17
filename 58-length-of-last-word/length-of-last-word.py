8015d984cb78b27672bf568e9e99f5839e89dc93
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-07-17 07:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (78.66%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -4,4 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        return len(s.split()[-1])
+        # method 1: return len(s.split()[-1])
+        
+        # Method 2:
+        words = s.strip().split()
+
+        if(not words):
+            return 0
+        return len(words[-1])    
