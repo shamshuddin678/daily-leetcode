5bbc6595851d017c4c739fca808a5bc9520163ad
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-07-22 19:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (82.72%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -3,57 +3,36 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution(object):
     def oddEvenList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        if(head == None or head.next == None):
+        if head is None:
             return head
 
-        ''' 
-        Brute force solution : 
         arr = []
-        # Here this jump for odd index data
+
+        # Collect odd-position values
         temp = head
-        while(temp != None and temp.next != None):
+        while temp is not None:
             arr.append(temp.val)
+            if temp.next is None:
+                break
             temp = temp.next.next
-        if(temp): # This is for last node data
-            arr.append(temp.val)
-        
-        # Here this is for even index data
+
+        # Collect even-position values
         temp = head.next
-        while(temp != None and temp.next != None):
+        while temp is not None:
             arr.append(temp.val)
+            if temp.next is None:
+                break
             temp = temp.next.next
-        if(temp): # This is for last node data 
-            arr.append(temp.val)
-        i = 0
+
+        # Write values back to the list
         temp = head
-        while(temp != None):
+        i = 0
+        while temp is not None:
             temp.val = arr[i]
             i += 1
             temp = temp.next
-        return head  '''
 
-        # Optimised solution
-        odd = head
-        even = head.next
-        even_head = head.next
-        
-        # Here condition is the : if even has reached the end or not . if even has reached the end then odd also reach
-        while(even != None and even.next != None):
-            #  Here change the links
-            odd.next = odd.next.next 
-            even.next = even.next.next
-
-            #  pointers moving
-            odd = odd.next
-            even = even.next
-            
-            #  Here this is for the last link for odd to even
-        odd.next = even_head
-        return head        
-
+        return head
