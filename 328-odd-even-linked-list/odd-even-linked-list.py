c87b993f9197d457516973f0943c246537824ba1
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-07-22 19:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (82.72%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -3,36 +3,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution(object):
     def oddEvenList(self, head):
-        if head is None:
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+        if(head == None or head.next == None):
             return head
+        
+        
+        odd = head
+        even = head.next
+        even_head = head.next
 
-        arr = []
+        while(even != None and even.next != None):
+            odd.next = odd.next.next
+            # move the odd pointer
+            odd = odd.next
+            
+            even.next = even.next.next
+            # move the even pointer
+            even = even.next
 
-        # Collect odd-position values
-        temp = head
-        while temp is not None:
-            arr.append(temp.val)
-            if temp.next is None:
-                break
-            temp = temp.next.next
-
-        # Collect even-position values
-        temp = head.next
-        while temp is not None:
-            arr.append(temp.val)
-            if temp.next is None:
-                break
-            temp = temp.next.next
-
-        # Write values back to the list
-        temp = head
-        i = 0
-        while temp is not None:
-            temp.val = arr[i]
-            i += 1
-            temp = temp.next
-
-        return head
+        # Here this for the last link
+        odd.next = even_head
+        return head 
