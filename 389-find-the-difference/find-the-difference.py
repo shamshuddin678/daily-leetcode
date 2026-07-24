f6229ed91e7f1c4052d8283d2de0ad618616f664
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-07-24 12:56:25 +0530

    Time: 3 ms (65.24%) | Memory: 12.3 MB (94.00%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
+        '''
         total = 0
         diff = 0
 
@@ -14,4 +15,12 @@ class Solution(object):
         for c in s:
             diff += ord(c) - ord('a')
 
-        return chr(total - diff + ord('a'))
+        return chr(total - diff + ord('a'))'''
+        fre = {}
+        for ch in t:
+            fre[ch] = fre.get(ch, 0) + 1
+        for ch in s:
+            fre[ch] -= 1
+        for ch, c in fre.items():
+            if c > 0:
+                return ch
