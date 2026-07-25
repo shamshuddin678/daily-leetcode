a393a7bd28bfc7d587e718584e1908a4ad3a31cb
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-07-25 14:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (79.04%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -11,25 +11,24 @@ class Solution(object):
         :type right: int
         :rtype: Optional[ListNode]
         """
-        if not head or left == right:
+        if(not head or left == right):
             return head
-
+        
         dummy = ListNode(0)
         dummy.next = head
 
-        # Move prev to node before left
+        # Here prev pointer moves the before of left pointer
         prev = dummy
-        for _ in range(left - 1):
+        for _ in range(left-1):
             prev = prev.next
-
-        # Here onwards Start of reversing
+        
+        # Here  onwards reversing starts
         curr = prev.next
 
-        # Reverse links 
-        for _ in range(right - left):
+        #  Reverse the links
+        for _ in range(right-left):
             temp = curr.next
             curr.next = temp.next
             temp.next = prev.next
             prev.next = temp
-
         return dummy.next
