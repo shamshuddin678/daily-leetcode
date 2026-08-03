e72bcf3b0ecef1cffa4634b21fd85d16169829a0
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-08-03 20:54:45 +0530

    Time: 31 ms (32.47%) | Memory: 25.9 MB (36.87%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,5 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # Here the one line 
-        return len(nums) != len(set(nums))
+        
+        '''Here the one line 
+        return len(nums) != len(set(nums))'''
+        # using the set 
+        seen = set()
+
+        for num in nums:
+            if(num in seen):
+                return True
+            seen.add(num)
+        return False
