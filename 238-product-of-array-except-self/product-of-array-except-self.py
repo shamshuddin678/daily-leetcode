d435fd1e3c95239a406392ef51b68e808c4fadaa
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-08-05 12:23:31 +0530

    Time: 47 ms (20.10%) | Memory: 23.5 MB (8.66%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,58 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        '''
-        Brute force:
-        ans = []
         n = len(nums)
-
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i != j:
-                    prod *= nums[j]
-            ans.append(prod)
-        return ans'''
-
-        #  Optimal solution for time complexity
-        n = len(nums)
-
         prefix = [1] * n
         suffix = [1] * n
-        ans = [1] * n
+        result = [1] * n
 
-        # Build Prefix Array
-        for i in range(1, n):
-            prefix[i] = prefix[i - 1] * nums[i - 1]
-
-        # Build Suffix Array
-        for i in range(n - 2, -1, -1):
-            suffix[i] = suffix[i + 1] * nums[i + 1]
-
-        # Build Answer
+        # for prefix sum
+        for i in range(1,n):
+            prefix[i] = prefix[i-1] * nums[i-1]
+        
+        # for suffix sum
+        for i in range(n-2,-1,-1):
+            suffix[i] = suffix[i+1] * nums[i+1]
+        
+        # for result array
         for i in range(n):
-            ans[i] = prefix[i] * suffix[i]
-
-        return ans   
+            result[i] = prefix[i] * suffix[i]
+        return result 
         
-        
-        
-        
-        
-        
-        
-        '''# optimal solution space complexity = O(1)
-        n = len(nums)
-        ans = [1] * n
-
-        # Prefix
-        for i in range(1, n):
-            ans[i] = ans[i-1] * nums[i-1]
-
-        # Suffix
-        suffix = 1
-        for i in range(n-2, -1, -1):
-            suffix *= nums[i+1]
-            ans[i] *= suffix
-
-        return ans  '''
