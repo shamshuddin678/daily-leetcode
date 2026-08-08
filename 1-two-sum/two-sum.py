c941767949bda14d697ca88492b47b7c0640590a
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-08-08 17:16:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (36.33%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -7,8 +7,15 @@ class Solution(object):
         """
         n = len(nums)
 
-        # Brute force
+        ''' Brute force
         for i in range(n):
             for j in range(i+1,n):
                 if(nums[i] + nums[j] == target):
-                    return i,j
+                    return i,j'''
+        # Optimal Solution using the Hashmap
+        hashmap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if(diff in hashmap):
+                return [hashmap[diff],i]
+            hashmap[nums[i]] = i
