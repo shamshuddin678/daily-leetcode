0ea073f4fa6481a040cb7cf3da6082290267dece
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-08-18 06:40:34 +0530

    Time: 43 ms (91.41%) | Memory: 19.1 MB (51.50%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: float
         """
-        # compute the sum of thw first window
-        win_sum = sum(nums[:k])
+        # first window sum
+        win_sum = sum(nums[ : k])
         max_sum = win_sum
 
-        # Slide the window : add the new element (left) and remove the old element
+        # slides the window
         for i in range(k,len(nums)):
             win_sum += nums[i] - nums[i-k]
             if(win_sum > max_sum):
                 max_sum = win_sum
-        return max_sum/ float(k)
+        return max_sum / float(k)
