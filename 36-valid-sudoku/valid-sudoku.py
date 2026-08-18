b720eb607593ba67f25cb64ccfd409395f558468
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-08-18 07:48:21 +0530

    Time: 7 ms (54.68%) | Memory: 12.3 MB (59.70%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        row_sets = [set() for _ in range(9)]
+        col_sets = [set() for _ in range(9)]
+        subgrid_sets = [
+            [set() for _ in range(3)] for _ in range(3)
+        ]
+
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+                if(num == "."):
+                    continue
+                if(num in row_sets[r]):
+                    return False
+                if(num in col_sets[c]):
+                    return False
+                if(num in subgrid_sets[r // 3][c // 3]):
+                    return False
+                row_sets[r].add(num)
+                col_sets[c].add(num)
+                subgrid_sets[r // 3][c // 3].add(num)
+        return True
