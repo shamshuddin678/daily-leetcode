84a0e44849362fe80fbaf08faab76d128dbe03a7
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-08-22 19:59:14 +0530

    Time: 42 ms (87.69%) | Memory: 21.3 MB (44.13%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,9 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        currmax = maxsofar = nums[0]
-        #  Kaden's array traversal
+        # Kadanes' with indices
+        current_sum = nums[0]
+        max_sumsofar = nums[0]
+
+        start = 0
+        best_start = 0
+        best_end = 0
+
         for i in range(1,len(nums)):
-            currmax = max(nums[i],currmax + nums[i])
-            maxsofar = max(maxsofar,currmax)
-        return maxsofar
+            if(nums[i] > current_sum + nums[i]):
+                current_sum = nums[i]
+                start = i
+            else:
+                current_sum += nums[i]
+            
+            # for boundaries when we find the new max 
+            if(current_sum > max_sumsofar):
+                max_sumsofar = current_sum
+                best_start = start
+                best_end = i
+        subarray = nums[best_start : best_end + 1]
+        return max_sumsofar 
