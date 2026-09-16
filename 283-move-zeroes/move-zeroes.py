cd6caa6dc277dafaebad0a81648b901fb7f9d521
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-09-16 22:11:10 +0530

    Time: 11 ms (25.81%) | Memory: 13.6 MB (20.30%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        non_zero = 0
-        for i in range(len(nums)):
-            if(nums[i] != 0):
-                nums[i],nums[non_zero] = nums[non_zero],nums[i]
-                non_zero += 1
-        return nums        
+        slow = 0
+        for fast in range(len(nums)):
+            if(nums[fast] != 0):
+                nums[slow],nums[fast] = nums[fast], nums[slow]
+                slow += 1
+        return nums
