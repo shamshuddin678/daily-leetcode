e3ec9a86a05e07e17e33a0beb419796e78cc751a
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-09-18 14:24:12 +0530

    Time: 4 ms (4.42%) | Memory: 43.6 MB (5.24%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,10 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();   
+        // return s.toLowerCase();
+        String ans = "";   
+        for(int i = 0; i< s.length(); i++){
+            ans += Character.toLowerCase(s.charAt(i));
+        }
+        return ans;
     }
 }
