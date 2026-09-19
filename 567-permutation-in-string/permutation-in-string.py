9fb5889a23f539e25cc8a0adcf480881ae7c7a81
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-09-19 07:46:57 +0530

    Time: 55 ms (23.05%) | Memory: 19.5 MB (19.24%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,11 +1,6 @@
-from collections import Counter
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
+from  collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
         need = Counter(s1)
         window = Counter()
         left = 0
@@ -13,10 +8,8 @@ class Solution(object):
             window[s2[right]] += 1
             if(right - left + 1 > len(s1)):
                 window[s2[left]] -= 1
-
-                if(window[s2[left]] == 0):
-                    del window[s2[left]]
                 left += 1
             if(window == need):
                 return True
         return False
+        
